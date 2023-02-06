59cd84b5718674420421ed6218b81e3990968539
Thulasiram Gunipati <tulasiram.gunipati@gmail.com>
2023-02-06 09:53:33 +0530

    test: Added new test for removing special chars function

--- tests/test_cleanml.py
+++ tests/test_cleanml.py
@@ -1,4 +1,4 @@
-from cleanml.cleanml import make_column_names
+from cleanml.cleanml import make_column_names, remove_spl_chars_in_columns
 import pandas as pd
 
 def test_make_column_names():
@@ -9,5 +9,16 @@ def test_make_column_names():
     data = pd.DataFrame(data_to_read)
     expected = ['name','age_of_children','school_name']
     data_new = make_column_names(data, column_dict={'Name of School (Mention full name)':'school_name'})
+    actual = data_new.columns.to_list()
+    assert expected == actual, "Column names conversion is incorrect!"
+
+def test_remove_spl_chars_in_columns():
+    data_to_read = {'child_name_(mention_full_name??)':['cheeka','cherry'],
+                'age_%':[14,10],
+                'name_of_School_(mention_full_name_&*!)':['InOrbit','InOrbit'],
+                }
+    data_new = pd.DataFrame(data_to_read)
+    expected = ['child_name_mention_full_name', 'age','name_of_School_mention_full_name']
+    data_new = remove_spl_chars_in_columns(data_new, spl_chars_excepted=['_'])
     actual = data_new.columns.to_list()
     assert expected == actual, "Column names conversion is incorrect!"
